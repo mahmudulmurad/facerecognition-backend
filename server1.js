9cafb0541cd15344e7969d803cd78973825cab46
mahmudulmurad <mahmudul.murad7@gmail.com>
2020-07-06 23:08:26 +0600

    changing URL

--- server1.js
+++ server1.js
@@ -11,7 +11,7 @@ const profile=require('./controller/profile.js');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
+    host : process.env.DATABASE_URL,
     ssl:true,
   }
 });
